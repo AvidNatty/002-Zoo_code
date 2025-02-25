bdaee39c812b60ef0f2e401a2d7f7cc5c0e0cc0b
Avidnatty <avidnatty@gmail.com>
2025-02-25 07:18:47 +0200

    1st commit

---
+++ Animal.java
@@ -0,0 +1,11 @@
+public class Animal{
+String name;
+String breed;
+int age;
+String colour;
+public void sound(){
+}
+public static void main(String[]args){
+Animal ani=new Animal();
+}
+}
---
+++ Employee.java
@@ -0,0 +1,12 @@
+public class Employee{
+String name;
+int idNumber;
+String address;
+double salary;
+int contactNumber;
+public void bookTicket(){
+}
+public static void main(String[]args){
+Employee employee=new Employee();
+}
+}
